60159b959658250543a69c202f1170b7b2d4392d
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-05-23 16:34:27 -0300

    Finish script and responsive design

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-//elements
+// Elements
 const generatePasswordButton = document.querySelector("#generate-password");
-const generatedPasswordButton = document.querySelector("#generated-password");
- // functions
+const generatedPasswordElement = document.querySelector("#generated-password");
+const passwordInput = document.querySelector("#password");
 
+// Functions
 const getLetterLowerCase = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
@@ -12,11 +13,42 @@ const getLetterUpperCase = () => {
 };
 
 const getNumber = () => {
-  return Math.floor(Math.random() * 10 ).toString();
+  return Math.floor(Math.random() * 10).toString();
 };
 
-//events
+const getSymbol = () => {
+  const symbols = "&/()[]{}&*+<>%=.,$#-";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
 
+const generatePassword = (
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol
+) => {
+  let password = "";
+  const passwordLength = 10;
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  ];
+
+  for (let i = 0; i < passwordLength; i += 1) {
+    const randomValue =
+      generators[Math.floor(Math.random() * generators.length)]();
+    password += randomValue;
+  }
+
+  password = password.slice(0, passwordLength);
+  generatedPasswordElement.style.display = "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
+  passwordInput.value = password;
+};
+
+// Events
 generatePasswordButton.addEventListener("click", () => {
-  console.log("teste")
-})
+  generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
+});
